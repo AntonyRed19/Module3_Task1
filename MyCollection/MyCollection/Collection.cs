714daec773e2ca7378a3b00a9678fd1582cd0dfc
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-03-16 13:30:12 +0200

    add Collection iterator

---
+++ MyCollection/MyCollection/Collection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection
+{
+   public class Collection : IEnumerable
+    {
+        private readonly CollectionEnumerator _enumerator;
+        private int[] _numbers = { 1, 2, 3, 4 };
+        public Collection(int[] numbers)
+        {
+            numbers = _numbers;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new CollectionEnumerator(_numbers);
+        }
+    }
+}
---
+++ MyCollection/MyCollection/Services/Abstractons/ICollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCollection.Services.Abstractons
+{
+    public interface ICollection
+    {
+        public void Sort();
+        public void RemoveAt();
+        public void Remove();
+        public void Add();
+        public void AddRange();
+    }
+}

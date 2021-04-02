3f14749014c11221b17114c3da5333f42d1f069e
DESKTOP-GCCS405\Antony <anton.redy19@gmail.com>
2021-04-02 14:39:47 +0300

    added myCollection

--- MyCollection/MyCollection/Collection.cs
+++ MyCollection/MyCollection/Collection.cs
@@ -7,18 +7,173 @@ using System.Threading.Tasks;
 
 namespace MyCollection
 {
-   public class Collection : IEnumerable
+    public class Collection<T> : IReadOnlyCollection<T>
     {
-        private readonly CollectionEnumerator _enumerator;
-        private int[] _numbers = { 1, 2, 3, 4 };
-        public Collection(int[] numbers)
+        private const int DefaultCount = 4;
+        private T[] _array;
+        private int _capacity;
+        public Collection(int capacity)
         {
-            numbers = _numbers;
+            _capacity = capacity;
+            _array = new T[_capacity];
         }
 
-        public IEnumerator GetEnumerator()
+        public Collection()
+            : this(DefaultCount)
         {
-            return new CollectionEnumerator(_numbers);
+        }
+
+        public int Count { get; private set; }
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+            set
+            {
+                if (!ValidateCapacity(value))
+                {
+                    return;
+                }
+
+                _capacity = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (Count == Capacity)
+            {
+                var isSuccess = CreateNewArray();
+                if (!isSuccess)
+                {
+                    return;
+                }
+            }
+
+            AddItem(item);
+        }
+
+        public void AddRange(IReadOnlyCollection<T> array)
+        {
+            var capacity = Count + array.Count;
+
+            if (capacity >= Capacity)
+            {
+                var isSuccess = CreateNewArray(capacity);
+                if (!isSuccess)
+                {
+                    return;
+                }
+            }
+
+            foreach (T item in array)
+            {
+                AddItem(item);
+            }
+        }
+
+        public bool RemoveAt(int index)
+        {
+            if (index >= Count || index < 0)
+            {
+                return false;
+            }
+
+            var lastIndext = Count - 1;
+
+            for (var i = index; i < lastIndext; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _array[lastIndext] = default(T);
+            Count--;
+
+            return true;
+        }
+
+        public bool Remove(T item)
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                if (_array[i].Equals(item))
+                {
+                    var isSuccess = RemoveAt(i);
+                    if (!isSuccess)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void Sort(IComparer<T> comparer)
+        {
+            Array.Sort(_array, comparer);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetGenericEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetGenericEnumerator();
+        }
+
+        private bool ValidateCapacity(int capacity)
+        {
+            return capacity > Count;
+        }
+
+        private void AddItem(T item)
+        {
+            _array[Count] = item;
+            Count++;
+        }
+
+        private bool CreateNewArray(int? capacity = null)
+        {
+            var tempArray = _array;
+            if (capacity == null)
+            {
+                _capacity = _capacity * 2;
+            }
+            else
+            {
+                var isValidCapacity = ValidateCapacity(capacity.Value);
+                if (!isValidCapacity)
+                {
+                    return false;
+                }
+
+                _capacity = capacity.Value;
+            }
+
+            _array = new T[Capacity];
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+
+            return true;
+        }
+
+        private IEnumerator<T> GetGenericEnumerator()
+        {
+            foreach (var item in _array)
+            {
+                if (!item.Equals(default(T)))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
--- MyCollection/MyCollection/Program.cs
+++ MyCollection/MyCollection/Program.cs
@@ -6,7 +6,35 @@ namespace MyCollection
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myList = new Collection<int>(5) { 1, 2, 5, 3 };
+            var array = new Collection<int> { 5, 2, 7, 5, 4, 9, 6, 5 };
+
+            myList.AddRange(array);
+            Console.WriteLine("Before remove");
+
+            Console.WriteLine($"Count = {myList.Count} Capacity = {myList.Capacity}");
+
+            foreach (var item in myList)
+            {
+                Console.Write($"{item} ");
+            }
+
+            myList.Remove(5);
+            myList.RemoveAt(5);
+
+            myList.Sort(new CollectionComparer());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("After remove & sort");
+
+            Console.WriteLine($"Count = {myList.Count} Capacity = {myList.Capacity}");
+
+            foreach (var item in myList)
+            {
+                Console.Write($"{item} ");
+            }
         }
     }
 }
